5f98c9cacffb08d6198e07f2c44c30316b79b199
asocastro <77965653+asocastro@users.noreply.github.com>
2024-05-29 18:31:20 +0800

    Revised Functions of match.py and search.py
    match.py:
    - Improved is_match() function to use pointers instead of iterating through the whole loops; also listed the measured changes as comments
    
    search.py:
    - added conditions to the search_users() function for params: id, name, age, occupation, so that it returns a list with unique results when searching the above mentioned params.

--- phasebook/match.py
+++ phasebook/match.py
@@ -19,9 +19,57 @@ def match(match_id):
     return {"message": msg, "elapsedTime": end - start}, 200
 
 
-def is_match(fave_numbers_1, fave_numbers_2):
+def is_match(fave_numbers_1, fave_numbers_2)->bool:
+    fave_numbers_1 = sorted(fave_numbers_1)
+    fave_numbers_2 = sorted(fave_numbers_2)
+    i = 0
+    j = 0
+    """
+    #Using pointers with element pop
+    while j < len(fave_numbers_2):
+        if i >= len(fave_numbers_1):
+            return False
+        if fave_numbers_1[i] == fave_numbers_2[j]:
+            j += 1
+            fave_numbers_1.pop(i)
+        else:
+            i += 1
+    return True
+    """
+    
+    #Using pointers  
+    while j < len(fave_numbers_2):  
+        if i >= len(fave_numbers_1):
+            return False
+        if fave_numbers_1[i] == fave_numbers_2[j]:
+            j += 1
+        i += 1
+    return True
+
+    """
+    #original
     for number in fave_numbers_2:
         if number not in fave_numbers_1:
             return False
-
     return True
+    """
+
+    """
+        First test on matches with no code changes:
+        match/0: 0.0 secs; Match Found
+        match/1: 0.0 secs; No Match
+        match/2: 39.06 secs; Match Found
+        match/3: 40.77 secs; No Match
+        
+        After Changes(Using pointers):
+        match/0: 0.0 secs; Match Found
+        match/1: 0.0 secs; No Match
+        match/2: 0.07 secs; Match Found
+        match/3: 0.08 secs; No Match
+        
+        After Changes(Using pointers + element pop):
+        match/0: 0.0 secs; Match Found
+        match/1: 0.0 secs; No Match
+        match/2: 1.11 secs; Match Found
+        match/3: 0.73 secs; No Match
+    """
--- phasebook/search.py
+++ phasebook/search.py
@@ -10,8 +10,7 @@ bp = Blueprint("search", __name__, url_prefix="/search")
 def search():
     return search_users(request.args.to_dict()), 200
 
-
-def search_users(args):
+def search_users(args) -> list:
     """Search users database
 
     Parameters:
@@ -24,7 +23,39 @@ def search_users(args):
     Returns:
         a list of users that match the search parameters
     """
+    
+    if not args:
+        return USERS
+    
+    matched_users = []
 
-    # Implement search here!
+    if 'id' in args:
+        user_id = args['id']
+        user = next((user for user in USERS if user["id"] == user_id), None)
+        if user:
+            matched_users.append(user)
 
-    return USERS
+    if 'name' in args:
+        name = args['name'].lower()
+        for user in USERS:
+            if name in user["name"].lower():
+                matched_users.append(user)
+
+    if 'age' in args:
+        age = int(args['age'])
+        for user in USERS:
+            if user["age"] in range(age - 1, age + 2):
+                matched_users.append(user)
+
+    if 'occupation' in args:
+        occupation = args['occupation'].lower()
+        for user in USERS:
+            if occupation in user["occupation"].lower():
+                matched_users.append(user)
+                
+    unique_users = []
+    for user in matched_users:
+        if user not in unique_users:
+            unique_users.append(user)
+
+    return unique_users
